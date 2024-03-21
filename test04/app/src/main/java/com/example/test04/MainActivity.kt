75d838db4cb896a30de11c8b4053d547d5e7d049
102214910 <160824695+vicwe@users.noreply.github.com>
2024-03-21 16:36:31 +0800

    test04

---
+++ test04/app/src/main/java/com/example/test04/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.test04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txtCom=findViewById<TextView>(R.id.txtCom)
+        val txtResult
+
+    }
+}
