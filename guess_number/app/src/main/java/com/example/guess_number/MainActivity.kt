1a6540fdf84cfa86189e3ca425f7b2245461a178
102214910 <160824695+vicwe@users.noreply.github.com>
2024-04-18 16:26:33 +0800

    0418

--- guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.guess_number.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView)
